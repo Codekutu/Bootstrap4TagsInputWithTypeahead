6b75e202a996d6a43123dcbb14259b805b57402f
Timothy Anthony <anthony.timothy90@gmail.com>
2020-04-26 18:02:01 +0300

    feat(App) Handling form input to show data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 /* App.js */
+var substringMatcher = function(strs) {
+    return function findMatches(q, cb) {
+        var matches;
+        matches = [];
+        substrRegex = new RegExp(q, 'i');
+        $.each(strs, function(i, str) {
+            if (substrRegex.test(str)) {
+                matches.push(str);
+            }
+        });
+        cb(matches);
+    };
+};
+
+var fruits = ['Apples', 'Avocado', 'Bananas', 'Blackberries', 'Blueberries', 
+                'Cherries', 'Cranberries', 'Durian', 'Grapefruit', 'Grapes', 
+                'Guava', 'Lemons', 'Mango', 'Olives', 'Oranges', 'Papaya', 
+                'Pineapple', 'Pomegranate', 'Strawberries', 'Watermelon'];
+
+$('#fruits').tagsinput({
+    typeaheadjs:({
+        hint: true,
+        highlight: true,
+        minLength: 1
+    }, {
+        name: 'fruits',
+        source: substringMatcher(fruits)
+    })
+});
